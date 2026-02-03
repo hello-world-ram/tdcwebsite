f3af413ded7458b79f3435c9733a07dd6676e45d
hello-world-ram <rampalgour932@gmail.com>
2026-02-03 21:28:54 +0530

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-document.getElementById("myForm").addEventListener("submit", function(e) {
-  e.preventDefault(); // stop page refresh
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-  const name = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
+  const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  if (!name || !email || !password) {
+  if (!username || !password) {
     alert("All fields are required!");
     return;
   }
